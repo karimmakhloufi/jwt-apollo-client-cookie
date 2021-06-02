38e3d1b0d4a6d23cb7e41a9a153a27650148867d
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-06-02 13:26:46 +0200

    Used mutation to get the token

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,30 @@
 import { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+
+const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+  }
+`;
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("admin@gmail.com");
+  const [password, setPassword] = useState("p4ssw0rd");
+  const [addTodo, { data }] = useMutation(LOGIN);
+  if (data) {
+    console.log(data);
+  }
   return (
     <>
       <input value={email} onChange={(e) => setEmail(e.target.value)} />
       <input value={password} onChange={(e) => setPassword(e.target.value)} />
       <button
-        onClick={() => {
-          console.log(email);
-          console.log(password);
+        onClick={async () => {
+          try {
+            await addTodo({ variables: { email: email, password: password } });
+          } catch (err) {
+            console.log("Handle me", err);
+          }
         }}
       >
         Login
