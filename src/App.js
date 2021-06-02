84de1d0ebae85d5ac7ab7f96b7675f458cd8c73b
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-06-02 12:27:19 +0200

    Installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Books from "./Books";
 
 const App = () => {
   return (
-    <div className="App">
-      <p>Hello world</p>
-      <Books />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <p>login</p>
+        </Route>
+        <Route path="/">
+          <Books />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
